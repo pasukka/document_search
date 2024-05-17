a0a4b89db2871713f79d788f6143dfd4edcefab4
PasukkaBig <o_kristina2000@mail.ru>
2024-05-18 05:23:38 +0700

    new var for docs path

--- document_searcher/config.py
+++ document_searcher/config.py
@@ -4,12 +4,14 @@ import yaml
 class Config:
     llm: str
 
-    def __init__(self, llm: str):
+    def __init__(self, llm: str, docs_path: str):
         self.llm = llm
+        self.docs_path = docs_path
 
 
 def load_config(file_path: str) -> Config:
     with open(file_path, 'r', encoding='utf-8') as stream:
         config_dict = yaml.safe_load(stream)['app-config']
 
-    return Config(config_dict['llm'])
+    return Config(config_dict['llm'], 
+                  config_dict['docs_path'])
