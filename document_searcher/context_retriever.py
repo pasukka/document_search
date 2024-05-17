fecc907c666d5ac3e535effe656c2f205b11fca2
PasukkaBig <o_kristina2000@mail.ru>
2024-05-18 05:20:28 +0700

    using langchain

--- document_searcher/context_retriever.py
+++ document_searcher/context_retriever.py
@@ -1,11 +1,11 @@
-from typing import Self
-# from transformers import AutoTokenizer, AutoModel
-# import torch
-# import torch.nn.functional as F
+import pandas as pd
+import os
 from os import listdir
 from os.path import isfile, join
-
-MIN_SIMILARITY = 0.3
+from langchain_community.document_loaders import DataFrameLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceHubEmbeddings
 
 
 class ContextRetriever:
@@ -13,57 +13,42 @@ class ContextRetriever:
     similarity_docs: list
     docs: list
 
-    def __init__(self):
+    def __init__(self, docs_path):
         super().__init__()
-        # self.bert = AutoModel.from_pretrained(self.model_id)
-        # self.tokenizer = AutoTokenizer.from_pretrained(self.model_id)
         self.similarity_docs = []
-        self.docs = []
-        path = "./documents/"
+        path = docs_path
         list_of_texts = [f for f in listdir(path) if isfile(join(path, f))]
 
         for i in range(len(list_of_texts)):
             with open(path + list_of_texts[i], 'r', encoding='utf-8') as file:
                 text = file.read()
-                self.docs.append({"id": i, "text": text})
-
-    # def vectorize_user_intent(self, text: str) -> list[float]:
-    #     input_ids = self.tokenizer(text, return_tensors='pt')['input_ids']
-    #     with torch.inference_mode():
-    #         result = self.forward(input_ids).squeeze()
-    #     values = [n.item() for n in result]
-    #     return values
-
-    # def forward(self, input_ids: torch.Tensor) -> torch.Tensor:
-    #     # [N, 768]
-    #     token_embeddings = self.bert(input_ids)[0]
-
-    #     # [1, 768]
-    #     token_embeddings_sums = torch.sum(token_embeddings, 1)
-
-    #     return F.normalize(token_embeddings_sums, p=2, dim=1)
+                self.similarity_docs.append({"id": i, "text": text})
 
     def __call__(self, user_intent: str) -> list[str]:
-        # query_vector = self.vectorize_user_intent(user_intent)
-        # self.search_docs(query_vector)
-        # context_list = [doc["content"] for doc in self.similarity_docs]
-        # return context_list
-        return ['Большой рюкзак продается в Спортмастере', 'Рюкзак Maybe One стоимостью 8 тыс. руб.']
+        # TODO: make summarization [doc["content"] for doc in self.similarity_docs]
+        context_list = self.search_docs(user_intent)
+        return context_list
 
-    def calculate_similarity(self, query_vector: list[float], doc_vector: list[float]) -> float:
-        res = 0
-        for a, b in zip(query_vector, doc_vector):
-            res += a * b
-        return res
+    def search_docs(self, query: str):
+        df = pd.DataFrame(self.similarity_docs)
+        df.head()
 
-    def search_docs(self, query_vector: list[float]):
-        for doc in self.docs:
-            similarity = self.calculate_similarity(query_vector, doc.vector())
+        loader = DataFrameLoader(df, page_content_column='text')
+        documents = loader.load()
 
-            if similarity < MIN_SIMILARITY:
-                continue
+        # TODO: change to summarization
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,
+                                                       chunk_overlap=0)
+        texts = text_splitter.split_documents(documents)
 
-            self.similarity_docs.append(doc)
+        embeddings = HuggingFaceHubEmbeddings(repo_id=self.model_id,
+                                              task="feature-extraction",
+                                              huggingfacehub_api_token=os.getenv('HUGGINGFACE_EMBEDDINGS_TOKEN'))
 
+        # making db
+        # TODO: save somewhere
+        db = FAISS.from_documents(texts, embeddings)
+        db.as_retriever()
 
-
+        embedding_vector = embeddings.embed_query(query)
+        return db.similarity_search_by_vector(embedding_vector)
