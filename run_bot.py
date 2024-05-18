7f9334cd55818d1956814519fab245ab5a7d338b
PasukkaBig <o_kristina2000@mail.ru>
2024-05-18 12:03:51 +0700

    working with tg bot

--- run_bot.py
+++ run_bot.py
@@ -1,8 +1,44 @@
+import telebot
+import os
+from dotenv import load_dotenv
+
 from chatbot.chatbot import DocumentSearchBot
 
+FIND_INFO = "Найти информацию"
+LOAD_FILE = "Загрузить файл"
+
+load_dotenv()
+bot = telebot.TeleBot(os.getenv('CHATBOT_KEY'))
+ds_bot = DocumentSearchBot()
+
+keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+button_info = telebot.types.KeyboardButton(text=FIND_INFO)
+button_import = telebot.types.KeyboardButton(text=LOAD_FILE)
+keyboard.add(button_info)
+keyboard.add(button_import)
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id,
+                     ds_bot.get_info(),
+                     parse_mode='Markdown',
+                     reply_markup=keyboard)
+
+
+@bot.message_handler(content_types=['text'])
+def handle_message(message):
+    if message.text == FIND_INFO:
+        bot.send_message(message.chat.id, 
+                         ds_bot.get_answer_message())
+    else:
+        answer = ds_bot.ask(message.text)
+        bot.send_message(message.chat.id, 
+                         answer)
+
 def main():
-    bot = DocumentSearchBot()
-    bot.start()
+    bot.polling(none_stop=True, interval=0)
+
 
 if __name__ == '__main__':
     main()
