42790ad796327cdb59af0adc14ef42a068d8f46b
PasukkaBig <o_kristina2000@mail.ru>
2024-05-19 22:05:54 +0700

    added some new questions for test

--- run_search.py
+++ run_search.py
@@ -4,6 +4,9 @@ from document_searcher.document_searcher import DocumentSearcher
 def main():
     chatbot = DocumentSearcher()
     chatbot.ask("Мне нужен большой надувной матрас. Какой бы ты посоветовал?")
+    chatbot.ask("Расскажи что-нибудь интересное.")
+    chatbot.ask("А что еще интересное знаешь?")
+    chatbot.ask("Расскажи про котиков")
 
 
 if __name__ == "__main__":
