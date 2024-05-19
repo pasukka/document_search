0d6ba5f7ce8ec63e801912772a73a7aad1fcf803
PasukkaBig <o_kristina2000@mail.ru>
2024-05-19 17:20:36 +0700

    changes in var's names and using funcs for responses

--- run_bot.py
+++ run_bot.py
@@ -5,37 +5,49 @@ from dotenv import load_dotenv
 
 from chatbot.chatbot import DocumentSearchBot
 
-FIND_INFO = "Найти информацию"
-LOAD_FILE = "Загрузить файл"
+# TODO: check for TimeoutError everywhere and Huggingface tokens
 
 load_dotenv()
 bot = telebot.TeleBot(os.getenv('CHATBOT_KEY'))
 ds_bot = DocumentSearchBot()
-
-keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-button_info = telebot.types.KeyboardButton(text=FIND_INFO)
-button_import = telebot.types.KeyboardButton(text=LOAD_FILE)
-keyboard.add(button_info)
-keyboard.add(button_import)
+find_info = ds_bot.get_button_find_info()
+load_file = ds_bot.get_button_load_file()
+help = ds_bot.get_button_help()
 
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_info = telebot.types.KeyboardButton(text=find_info)
+    button_import = telebot.types.KeyboardButton(text=load_file)
+    button_help = telebot.types.KeyboardButton(text=help)
+    keyboard.row(button_info)
+    keyboard.row(button_import, button_help)
+
     ds_bot.restart()
     bot.send_message(message.chat.id,
-                     ds_bot.get_info(),
+                     ds_bot.get_start_info(),
                      parse_mode='Markdown',
                      reply_markup=keyboard)
 
 
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    bot.send_message(message.chat.id,
+                     ds_bot.get_help_info(),
+                     parse_mode='Markdown')
+
+
 @bot.message_handler(content_types=['text'])
 def handle_message(message):
-    if message.text == FIND_INFO:
+    if message.text == find_info:
         bot.send_message(message.chat.id,
-                         ds_bot.get_answer_message())
-    elif message.text == LOAD_FILE:
+                         ds_bot.get_find_message())
+    elif message.text == load_file:
         bot.send_message(message.chat.id,
-                         'Жду загрузку файла!')
+                         ds_bot.load_file_response())
+    elif message.text == help:
+        handle_help(message)
     else:
         try:
             answer = ds_bot.ask(message.text)
@@ -43,38 +55,37 @@ def handle_message(message):
                              answer)
         except ValueError:
             bot.send_message(message.chat.id,
-                             'Возникла ошибка. Повторите запрос позже.')
+                             ds_bot.error_token_response())
         except TimeoutError:
             time.sleep(2)
             handle_message(message)
         except Exception as e:
             bot.send_message(message.chat.id,
-                             'Возникла ошибка. Повторите запрос еще раз.')
+                             ds_bot.unknown_error_response())
             pass
 
 
 @bot.message_handler(content_types=['document'])
-def load(message):
+def load_document(message):
     if 'txt' == message.document.file_name.split('.')[1]:
         try:
             file_info = bot.get_file(message.document.file_id)
             downloaded_file = bot.download_file(file_info.file_path)
+            bot.send_message(message.chat.id,
+                             ds_bot.waiting_for_loading_response())
             try:
+                ds_bot.load_file(message.from_user.id,
+                                    message.document.file_name, downloaded_file)
                 bot.send_message(message.chat.id,
-                                'Подождите, я загружаю файл...')
-                ds_bot.load_file(message.document.file_name, downloaded_file)
-                bot.send_message(message.chat.id,
-                                'Загрузка прошла успешно!')
+                                ds_bot.success_file_loading_response())
             except Exception as e:
                 bot.send_message(message.chat.id,
-                             'Загрузка прервана!')
+                                 ds_bot.loading_file_error_response())
                 bot.send_message(message.chat.id, e)
         except Exception as e:
             bot.reply_to(message, e)
-
-        # TODO: MAKE check for TimeoutError everywhere and Huggingface tokens
     else:
-        bot.send_message("Неправильный формат файла")
+        bot.send_message(ds_bot.error_file_format_response())
 
 
 def main():
