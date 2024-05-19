e84342858c8c22a57a1662eee0a07d0ed96ef82f
PasukkaBig <o_kristina2000@mail.ru>
2024-05-19 17:13:09 +0700

    loading file to dir by chat_id, deleting files while restart, funcs for metadata, new comments

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,35 +1,84 @@
+import os
+import json
+
 from document_searcher.document_searcher import DocumentSearcher
 
-# TODO: make /help;
-# TODO: add that for searching only one text need to /start again (need to do for 1 or for many docs)
-# TODO: for each user we need to have new name of dir f their files (by id)
-
+# TODO: different strategies for searching one text or many docs
 # TODO: mb add some comments
+# TODO: do smth with big amount of funcs
+
+# TODO: add legal stopping program
+# TODO: take 'user_dir_path' from bot or when stopping program clean everything
 
 
 class DocumentSearchBot:
 
     def __init__(self):
-        self.ds = DocumentSearcher()
+        self.doc_searcher = DocumentSearcher()
+        self.docs_path = self.doc_searcher.docs_path
+        self.user_dir_path = ''
+        with open('metadata/metadata.json', 'r', encoding='utf-8') as file:
+            self.metadata = json.load(file)
 
-    def get_info(self) -> str:
-        info_str = f"Добро пожаловать в бот для поиска информации по документу!\n" \
-            "Если у тебя есть вопрос, задай его или нажми *Найти информацию*.\n" \
-            "Для загрузки файла, по которому необходимо проводить поиск нажми *Загрузить файл*."
-        return info_str
+    def get_start_info(self) -> str:
+        return self.metadata["info"]["start_info"]
 
-    def get_answer_message(self) -> str:
-        return f'Отлично! Что необходимо найти?'
+    def get_help_info(self) -> str:
+        return self.metadata["info"]["help_info"]
+
+    def get_button_find_info(self) -> str:
+        return self.metadata["button"]["button_find_info"]
+
+    def get_button_load_file(self) -> str:
+        return self.metadata["button"]["button_load_file"]
+
+    def get_button_help(self) -> str:
+        return self.metadata["button"]["button_help"]
+
+    def get_find_message(self) -> str:
+        return self.metadata["response"]["find_response"]
+
+    def load_file_response(self) -> str:
+        return self.metadata["response"]["load_file_response"]
+
+    def waiting_for_loading_response(self) -> str:
+        return self.metadata["response"]["loading_file_response"]
+
+    def success_file_loading_response(self) -> str:
+        return self.metadata["response"]["file_loaded_response"]
+
+    def error_file_format_response(self) -> str:
+        return self.metadata["error"]["error_file_format"]
+
+    def loading_file_error_response(self) -> str:
+        return self.metadata["error"]["loading_file_error"]
+
+    def error_token_response(self) -> str:
+        return self.metadata["error"]["error_token_response"]
+
+    def unknown_error_response(self) -> str:
+        return self.metadata["error"]["unknown_error_response"]
 
     def ask(self, message: str) -> str:
-        answer = self.ds.ask(message)
+        answer = self.doc_searcher.ask(message)
         return answer
 
     def restart(self):
-        self.ds.restart()
+        if os.path.exists(self.user_dir_path):
+            filelist = [f for f in os.listdir(self.user_dir_path)]
+            try:
+                for f in filelist:
+                    os.remove(os.path.join(self.user_dir_path, f))
+                os.rmdir(self.user_dir_path)
+            except Exception:
+                pass
+        self.doc_searcher.restart()
 
-    def load_file(self, document_file_name, downloaded_file):
-        src = self.ds.new_docs_path + document_file_name
+    def load_file(self, chat_id, document_file_name, downloaded_file):
+        self.user_dir_path = self.docs_path + 'chat_' + str(chat_id) + '/'
+        if not os.path.exists(self.user_dir_path):
+            os.makedirs(self.user_dir_path)
+        src = self.user_dir_path + document_file_name
         with open(src, 'wb') as new_file:
             new_file.write(downloaded_file)
-        self.ds.change_docs_path()
+        self.doc_searcher.change_docs_path(self.user_dir_path)
