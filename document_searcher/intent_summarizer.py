5ce1b5312f0778b27c9cfc4afcd9438734929e4b
PasukkaBig <o_kristina2000@mail.ru>
2024-05-19 17:14:36 +0700

    changes in vars names

--- document_searcher/intent_summarizer.py
+++ document_searcher/intent_summarizer.py
@@ -2,9 +2,11 @@ import os
 from huggingface_hub import InferenceClient
 from document_searcher.config import load_config
 
+# TODO: work with intent more for less hallucinating 
 
 class IntentSummarizer:
     hf_token: str
+    model: str
 
     def __init__(self):
         self.hf_token = os.getenv('HUGGINGFACE_INTENT_TOKEN')
@@ -13,12 +15,11 @@ class IntentSummarizer:
         self.llm = InferenceClient(model=self.model,
                                    timeout=8,
                                    token=self.hf_token)
-        prompt_path = "./prompts/user_intent.txt"
-        with open(prompt_path, 'r', encoding='utf-8') as file:
-            self.template_prompt = file.read()
+        with open("./prompts/user_intent.txt", 'r', encoding='utf-8') as file:
+            self.prompt_template = file.read()
 
     def __call__(self, query: str, chat_history: str) -> str:
-        prompt = self.template_prompt.replace('{chat_history}', chat_history)
+        prompt = self.prompt_template.replace('{chat_history}', chat_history)
         prompt = prompt.replace('{query}', query)
         response = self.llm.text_generation(prompt,
                                             do_sample=False,
