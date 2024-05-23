6821e7a536edeca9d8d9036b53b8f8d26898b150
PasukkaBig <o_kristina2000@mail.ru>
2024-05-23 12:39:36 +0700

    test for thanks

--- run_search.py
+++ run_search.py
@@ -5,11 +5,7 @@ def main():
     chatbot = DocumentSearcher()
     chatbot.ask("Мне нужен большой надувной матрас. Какой бы ты посоветовал?")
     chatbot.ask("Расскажи что-нибудь интересное.")
-    chatbot.ask("А что еще интересное знаешь?")
-    chatbot.ask("Расскажи про котиков")
-    chatbot.ask("Расскажи что-нибудь интересное.") # TODO gets code 3; need to to find a good way for fixing
-    chatbot.ask("А что еще интересное знаешь?")
-    chatbot.ask("Расскажи про котиков")
+    chatbot.ask("Спасибо")
 
 
 if __name__ == "__main__":
