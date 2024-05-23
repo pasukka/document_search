58868d62193ce82e60bb1f99dc9c8f82f7915cb8
PasukkaBig <o_kristina2000@mail.ru>
2024-05-23 16:55:28 +0700

    changed telebot for aiogram

--- run_bot.py
+++ run_bot.py
@@ -1,12 +1,21 @@
-import telebot
+import asyncio
+import logging
 import os
 from dotenv import load_dotenv
 
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.types import ContentType
+from aiogram.filters.command import Command
+
 from chatbot.chatbot import DocumentSearchBot
 
+# TODO: make /smth - some responses of what is wrong
 
 load_dotenv()
-bot = telebot.TeleBot(os.getenv('CHATBOT_KEY'))
+logging.basicConfig(level=logging.INFO)
+bot = Bot(os.getenv('CHATBOT_KEY'))
+dp = Dispatcher()
+
 ds_bot = DocumentSearchBot()
 find_info = ds_bot.get_button_find_info()
 load_file = ds_bot.get_button_load_file()
@@ -14,81 +23,67 @@ help = ds_bot.get_button_help()
 clean = ds_bot.get_button_clean()
 
 
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button_info = telebot.types.KeyboardButton(text=find_info)
-    button_import = telebot.types.KeyboardButton(text=load_file)
-    button_help = telebot.types.KeyboardButton(text=help)
-    button_clean = telebot.types.KeyboardButton(text=clean)
-    keyboard.row(button_info, button_import)
-    keyboard.row(button_help, button_clean)
+@dp.message(Command("start"))
+async def handle_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text=find_info),
+        types.KeyboardButton(text=load_file)],
+        [types.KeyboardButton(text=help),
+        types.KeyboardButton(text=clean)]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb,
+                                         resize_keyboard=True)
 
+    await message.answer(ds_bot.get_start_info(),
+                         parse_mode='Markdown',
+                         reply_markup=keyboard)
     ds_bot.restart(message.chat.id)
-    bot.send_message(message.chat.id,
-                     ds_bot.get_start_info(),
-                     parse_mode='Markdown',
-                     reply_markup=keyboard)
 
+@dp.message(Command("help"))
+async def handle_help(message: types.Message):
+    await message.answer(ds_bot.get_help_info(),
+                         parse_mode='Markdown')
+    
+@dp.message(Command("clean"))
+async def handle_clean(message: types.Message):
+    await message.answer(ds_bot.get_clean_info(),
+                         parse_mode='Markdown')
 
-@bot.message_handler(commands=['help'])
-def handle_help(message):
-    bot.send_message(message.chat.id,
-                     ds_bot.get_help_info(),
-                     parse_mode='Markdown')
+@dp.message(F.content_type == ContentType.DOCUMENT)
+async def handle_message(message: types.Message):
+    if 'txt' == message.document.file_name.split('.')[1]:
+        try:
+            file_info = await bot.get_file(message.document.file_id)
+            await message.answer(ds_bot.waiting_for_loading_response())
+            try:
+                path = ds_bot.get_path(message.chat.id)
+                await bot.download_file(file_info.file_path, path + message.document.file_name)
+                ds_bot.change_docs_path(message.chat.id)
+                await message.answer(ds_bot.success_file_loading_response())
+            except Exception as e:
+                await message.answer(ds_bot.loading_file_error_response())
 
+        except Exception as e:
+            message.reply(message, e)
+    else:
+        await message.answer(ds_bot.error_file_format_response())
 
-@bot.message_handler(commands=['clean'])
-def handle_clean(message):
-    ds_bot.clean_user_dir()
-    bot.send_message(message.chat.id,
-                     ds_bot.get_clean_info(),
-                     parse_mode='Markdown')
-
-
-@bot.message_handler(content_types=['text'])
-def handle_message(message):
+@dp.message()
+async def handle_message(message: types.Message):
     if message.text == find_info:
-        bot.send_message(message.chat.id,
-                         ds_bot.get_find_message())
+        await message.answer(ds_bot.get_find_message())
     elif message.text == load_file:
-        bot.send_message(message.chat.id,
-                         ds_bot.load_file_response())
+        await message.answer(ds_bot.load_file_response())
     elif message.text == help:
         handle_help(message)
     elif message.text == clean:
         handle_clean(message)
     else:
         answer = ds_bot.ask(message.text)
-        bot.send_message(message.chat.id, answer)
+        await message.answer(answer)
 
+async def main():
+    await dp.start_polling(bot)
 
-@bot.message_handler(content_types=['document'])
-def load_document(message):
-    if 'txt' == message.document.file_name.split('.')[1]:
-        try:
-            file_info = bot.get_file(message.document.file_id)
-            downloaded_file = bot.download_file(file_info.file_path)
-            bot.send_message(message.chat.id,
-                             ds_bot.waiting_for_loading_response())
-            try:
-                ds_bot.load_file(message.from_user.id,
-                                 message.document.file_name, downloaded_file)
-                bot.send_message(message.chat.id,
-                                 ds_bot.success_file_loading_response())
-            except Exception as e:
-                bot.send_message(message.chat.id,
-                                 ds_bot.loading_file_error_response())
-                bot.send_message(message.chat.id, e)
-        except Exception as e:
-            bot.reply_to(message, e)
-    else:
-        bot.send_message(ds_bot.error_file_format_response())
-
-
-def main():
-    bot.polling(none_stop=True, interval=0)
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    asyncio.run(main())
