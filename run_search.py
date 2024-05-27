84fe7e1db2f39c688836a908210c365c3d058984
PasukkaBig <o_kristina2000@mail.ru>
2024-05-27 22:01:36 +0700

    example of error

--- run_search.py
+++ run_search.py
@@ -1,8 +1,15 @@
 from document_searcher.document_searcher import DocumentSearcher
+import document_searcher
 
 
 def main():
     chatbot = DocumentSearcher()
+    try:
+        docs_path = 'no_dir/'
+        chatbot.change_docs_path(docs_path)
+    except document_searcher.errors.FileError as e:
+        print(e.message)
+        
     chatbot.ask("Мне нужен большой надувной матрас. Какой бы ты посоветовал?")
     chatbot.ask("Расскажи что-нибудь интересное.")
     chatbot.ask("Спасибо")
