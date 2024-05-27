d1cd24a135db950effabff9db419540fa9a75177
PasukkaBig <o_kristina2000@mail.ru>
2024-05-27 22:03:08 +0700

    added better error handeling4 added types in funcs

--- bot/search_manager.py
+++ bot/search_manager.py
@@ -4,6 +4,7 @@ import time
 import datetime
 import shutil
 
+import document_searcher
 from document_searcher.document_searcher import DocumentSearcher
 
 MAX_RETRIES = 2
@@ -20,13 +21,14 @@ class DocumentSearcherManager:
             self.metadata = json.load(file)
         # self.clean_all_user_dirs()
 
-    def get_error_message(self) -> str:
-        error = self.doc_searcher.error_code
+    def get_error_message(self, error: Exception) -> str:
         error_str = ""
-        if error == 1:
+        if type(error) == document_searcher.errors.TimeError:
             error_str = self.metadata["error"]["error_runtime"]
-        elif error == 2:
+        elif type(error) == document_searcher.errors.TokenError:
             error_str = self.metadata["error"]["error_token_response"]
+        elif type(error) == document_searcher.errors.FileError:
+            error_str = self.metadata["error"]["error_file"]
         else:
             error_str = self.metadata["error"]["unknown_error_response"]
         return error_str
@@ -34,31 +36,32 @@ class DocumentSearcherManager:
     def ask(self, message: str) -> str:
         answer = ""
         while answer == "" and self.retries < MAX_RETRIES:
-            answer = self.doc_searcher.ask(message)
-            self.retries += 1
-            time.sleep(4)
-        if answer == "":
-            answer = self.get_error_message()
+            try:
+                answer = self.doc_searcher.ask(message)
+                self.retries += 1
+                time.sleep(4)
+            except Exception as e:
+                answer = self.get_error_message(e)
         self.retries = 0
         return answer
 
-    def get_user_dir(self, chat_id):
+    def get_user_dir(self, chat_id: int) -> str:
         return self.docs_path + 'chat_' + str(chat_id) + '/'
 
-    def restart(self, chat_id):
+    def restart(self, chat_id: int) -> None:
         self.clean_user_dir(chat_id)
 
-    def change_docs_path(self, chat_id):
+    def change_docs_path(self, chat_id: int) -> None:
         user_dir_path = self.get_user_dir(chat_id)
         self.doc_searcher.change_docs_path(user_dir_path)
 
-    def get_path(self, chat_id):
+    def get_path(self, chat_id: int) -> str:
         user_dir_path = self.get_user_dir(chat_id)
         if not os.path.exists(user_dir_path):
             os.makedirs(user_dir_path)
         return user_dir_path
 
-    def get_docs_list(self, chat_id):
+    def get_docs_list(self, chat_id: int) -> list[str]:
         user_dir_path = self.get_user_dir(chat_id)
         filelist = []
         if os.path.exists(user_dir_path):
@@ -66,21 +69,21 @@ class DocumentSearcherManager:
                 user_dir_path) if f.split('.')[1] == 'txt']
         return filelist
 
-    def clean_all_user_dirs(self):
+    def clean_all_user_dirs(self) -> None:
         dirlist = [f for f in os.listdir(self.docs_path)
                    if f.startswith('chat_')]
         for dir in dirlist:
             self.clean_user_dir(user_dir=self.docs_path+dir)
 
-    def clean_user_dir(self, chat_id=None, user_dir=''):
+    def clean_user_dir(self, chat_id=None, user_dir='') -> None:
         if chat_id:
             user_dir = self.get_user_dir(chat_id)
         if os.path.exists(user_dir):
-           shutil.rmtree(user_dir)
+            shutil.rmtree(user_dir)
         self.doc_searcher.restart()
         self.doc_searcher.change_docs_path()
 
-    def remove_chosen_files(self, chat_id: int, filelist: list):
+    def remove_chosen_files(self, chat_id: int, filelist: list) -> None:
         user_dir = self.get_user_dir(chat_id)
         try:
             for f in filelist:
@@ -88,7 +91,7 @@ class DocumentSearcherManager:
             new_path = user_dir
             filelist = [f for f in os.listdir(user_dir)
                         if f.split('.')[1] == 'txt']
-            if len(filelist) == 0: 
+            if len(filelist) == 0:
                 self.doc_searcher.restart()
                 new_path = self.doc_searcher.docs_path
                 shutil.rmtree(user_dir)
@@ -96,7 +99,7 @@ class DocumentSearcherManager:
         except Exception:
             pass
 
-    def callback(self, message):
+    def callback(self, message: str) -> None:
         t = datetime.datetime.now()
         with open(self.log_path, 'a+', encoding='utf-8') as f:
             f.write(f'Time:{t}\nMessage:{message}\n\n')
