ace6661aeabf16fb137b3ee8ca3d4fa1d147366d
PasukkaBig <o_kristina2000@mail.ru>
2024-05-27 18:06:59 +0700

    renamed attr

--- document_searcher/intent_summarizer.py
+++ document_searcher/intent_summarizer.py
@@ -4,16 +4,17 @@ from document_searcher.config import load_config
 
 
 class IntentSummarizer:
-    hf_token: str
+    _hf_token: str
     model: str
+    prompt_template: str
 
     def __init__(self):
-        self.hf_token = os.getenv('HUGGINGFACE_INTENT_TOKEN')
+        self._hf_token = os.getenv('HUGGINGFACE_INTENT_TOKEN')
         config = load_config('config.yml')
         self.model = config.llm
         self.llm = InferenceClient(model=self.model,
                                    timeout=100,
-                                   token=self.hf_token)
+                                   token=self._hf_token)
         with open("./prompts/user_intent.txt", 'r', encoding='utf-8') as file:
             self.prompt_template = file.read()
 
