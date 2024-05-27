ce42bfa37921b89419402756f9539f8db30ff029
PasukkaBig <o_kristina2000@mail.ru>
2024-05-27 22:03:29 +0700

    added types in funcs

--- document_searcher/context_retriever.py
+++ document_searcher/context_retriever.py
@@ -12,7 +12,7 @@ class ContextRetriever:
     model: str
     documents_path: str
 
-    def __init__(self, documents_path):
+    def __init__(self, documents_path: str):
         self.model = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
         self.documents_path = documents_path
         self.documents_embeddings = HuggingFaceHubEmbeddings(repo_id=self.model,
@@ -26,22 +26,21 @@ class ContextRetriever:
             doc.page_content for doc in relevant_documents]
         return documents_context_list
 
-    def __load_data_base(self):
+    def __load_data_base(self) -> None:
         db_dir = self.documents_path
         if not os.path.exists(db_dir):
             raise FileNotFoundError('Директории не существует')
         if os.path.exists(db_dir+'index.faiss'):
             # loading needs allow_dangerous_deserialization
             self.db = FAISS.load_local(db_dir,
-                                    self.documents_embeddings,
-                                    allow_dangerous_deserialization=True)
+                                       self.documents_embeddings,
+                                       allow_dangerous_deserialization=True)
         else:
             self.reload_db(db_dir)
 
-
-    def reload_db(self, path):
-        self.documents_path = path
+    def reload_db(self, path: str) -> None:
         documents = self.__load_documents(path)
+        self.documents_path = path
         df = pd.DataFrame(documents)
         loader = DataFrameLoader(df, page_content_column='text')
         loaded_documents = loader.load()
@@ -51,13 +50,13 @@ class ContextRetriever:
                                                        chunk_overlap=0)
         splitted_documents = text_splitter.split_documents(
             loaded_documents)
-        
+
         # [Document(page_content='text', metadata={id:1})]
         self.db = FAISS.from_documents(splitted_documents,
                                        self.documents_embeddings)
         self.db.save_local(path)
 
-    def __load_documents(self, path):
+    def __load_documents(self, path: str) -> list[dict]:
         list_of_documents = [f for f in listdir(path)
                              if isfile(join(path, f)) and f.split('.')[1] == 'txt']
         documents = []
@@ -67,7 +66,7 @@ class ContextRetriever:
                 documents.append({"id": i, "text": text})
         return documents
 
-    def __find_relevant_documents(self, query: str):
+    def __find_relevant_documents(self, query: str) -> list:
         embedding_vector = self.documents_embeddings.embed_query(query)
         relevant_docs = self.db.similarity_search_by_vector(embedding_vector)
         return relevant_docs
