38c0615c6ab3d79b6153592aca67afd56511c58e
PasukkaBig <o_kristina2000@mail.ru>
2024-06-01 17:41:32 +0700

    logging errors and info

--- bot/search_manager.py
+++ bot/search_manager.py
@@ -6,6 +6,7 @@ import shutil
 
 import document_searcher
 from document_searcher.document_searcher import DocumentSearcher
+from loggers.loggers import BotLogger, CallbackLogger
 
 MAX_RETRIES = 2
 
@@ -16,24 +17,30 @@ class DocumentSearcherManager:
         self.doc_searcher = DocumentSearcher()
         self.docs_path = self.doc_searcher.docs_path
         self.retries = 0
-        self.log_path = "user_errors.log"
+        self.bot_logger = BotLogger()
+        self.user_feedback_logger = CallbackLogger()
+        self.log_path = "logs/user_errors.log"
         with open('metadata/metadata.json', 'r', encoding='utf-8') as file:
             self.metadata = json.load(file)
         # self.clean_all_user_dirs()
 
-    def get_error_message(self, error: Exception) -> str:
+    def get_error_message(self, error: Exception, id: int) -> str:
         error_str = ""
         if type(error) == document_searcher.errors.TimeError:
             error_str = self.metadata["error"]["error_runtime"]
+            self.bot_logger.logger.error(f"Chat: {id} - RunTime error.")
         elif type(error) == document_searcher.errors.TokenError:
             error_str = self.metadata["error"]["error_token_response"]
+            self.bot_logger.logger.error(f"Chat: {id} - Token expired.")
         elif type(error) == document_searcher.errors.FileError:
             error_str = self.metadata["error"]["error_file"]
+            self.bot_logger.logger.error(f"Chat: {id} - File error.")
         else:
             error_str = self.metadata["error"]["unknown_error_response"]
+            self.bot_logger.logger.error(f"Chat: {id} - Error: {error}.")
         return error_str
 
-    def ask(self, message: str) -> str:
+    def ask(self, message: str, id: int) -> str:
         answer = ""
         while answer == "" and self.retries < MAX_RETRIES:
             try:
@@ -41,7 +48,7 @@ class DocumentSearcherManager:
                 self.retries += 1
                 time.sleep(4)
             except Exception as e:
-                answer = self.get_error_message(e)
+                answer = self.get_error_message(e, id)
         self.retries = 0
         return answer
 
@@ -75,15 +82,16 @@ class DocumentSearcherManager:
         for dir in dirlist:
             self.clean_user_dir(user_dir=self.docs_path+dir)
 
-    def clean_user_dir(self, chat_id=None, user_dir='') -> None:
+    def clean_user_dir(self, chat_id=None, user_dir='') -> str:
         if chat_id != None:
             user_dir = self._get_user_dir(chat_id)
         if os.path.exists(user_dir):
             shutil.rmtree(user_dir)
         self.doc_searcher.restart()
         self.doc_searcher.change_docs_path()
+        return user_dir
 
-    def remove_chosen_files(self, chat_id: int, filelist: list) -> None:
+    def remove_chosen_files(self, chat_id: int, filelist: list) -> str:
         user_dir = self._get_user_dir(chat_id)
         try:
             for f in filelist:
@@ -98,8 +106,11 @@ class DocumentSearcherManager:
             self.doc_searcher.change_docs_path(new_path)
         except Exception:
             pass
+        return user_dir
 
-    def callback(self, chat_id: str, message: str) -> None:
-        t = datetime.datetime.now()
-        with open(self.log_path, 'a+', encoding='utf-8') as f:
-            f.write(f'Time:{t}\nChat_id:{chat_id}\nMessage:{message}\n\n')
+    def callback(self, chat_id: str, message: str) -> str:
+        date_now = datetime.datetime.now()
+        id = f"{str(chat_id)}-{str(date_now.date())}-{str(date_now.hour)}-{str(date_now.minute)}"
+        self.user_feedback_logger.logger.warning(
+            f'Id: {id} - Chat_id: {chat_id}\nMessage: {message}\n\n')
+        return id
