0ed92adf2e83601a9429687e1a625115997eb2ec
PasukkaBig <o_kristina2000@mail.ru>
2024-06-01 17:40:08 +0700

    added logging starting and ending bot

--- run_bot.py
+++ run_bot.py
@@ -1,34 +1,36 @@
 import asyncio
-import logging
 import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram_dialog import Dialog
 from aiogram_dialog import setup_dialogs
 
-from bot.bot import router
+from bot.bot import router, bot_logger
 from bot.commands import set_commands
 from bot.windows import file_list_window, remove_files_window
 
 
 async def start():
     load_dotenv()
-    logging.basicConfig(level=logging.INFO)
     bot = Bot(os.getenv('CHATBOT_KEY'))
+    info = await bot.get_me()
+    bot_logger.logger.info(f"Started bot @{info.username} id={bot.id}.")
     dp = Dispatcher()
     dp.include_router(router)
     dialog = Dialog(file_list_window, remove_files_window)
     dp.include_router(dialog)
     setup_dialogs(dp)
     await set_commands(bot)
-
     try:
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
+        bot_logger.logger.critical(f"Closed bot @{info.username} id={bot.id}.")
 
 if __name__ == "__main__":
     try:
         asyncio.run(start())
     except KeyboardInterrupt:
         print("\nOkay, I'm stopping...")
+    finally:
+        bot_logger.logger.critical("Bot stopped.")
