2ec11d783f3a68f84e6e387bb96698afa2fb5334
PasukkaBig <o_kristina2000@mail.ru>
2024-06-01 19:33:55 +0700

    added loggers for searcher

--- loggers/loggers.py
+++ loggers/loggers.py
@@ -22,6 +22,54 @@ class BotLogger(Logger):
         self.logger.addHandler(self.file_handler)
 
 
+class ManagerLogger(Logger):
+
+    def __init__(self):
+        super().__init__()
+        self.logger = logging.getLogger('managerLogger')
+        self.file_handler = logging.FileHandler('logs/manager_logger.log',
+                                                mode="a+",
+                                                encoding="utf8")
+        self.file_handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.file_handler)
+
+
+class SearcherLogger(Logger):
+
+    def __init__(self):
+        super().__init__()
+        self.logger = logging.getLogger('searcherLogger')
+        self.file_handler = logging.FileHandler('logs/searcher_logger.log',
+                                                mode="a+",
+                                                encoding="utf8")
+        self.file_handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.file_handler)
+
+
+class IntentSummarizerLogger(Logger):
+
+    def __init__(self):
+        super().__init__()
+        self.logger = logging.getLogger('intentSummarizerLogger')
+        self.file_handler = logging.FileHandler('logs/intent_logger.log',
+                                                mode="a+",
+                                                encoding="utf8")
+        self.file_handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.file_handler)
+
+
+class ContextRetrieverLogger(Logger):
+
+    def __init__(self):
+        super().__init__()
+        self.logger = logging.getLogger('contextRetrieverLogger')
+        self.file_handler = logging.FileHandler('logs/context_logger.log',
+                                                mode="a+",
+                                                encoding="utf8")
+        self.file_handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.file_handler)
+
+
 class CallbackLogger(Logger):
 
     def __init__(self):
