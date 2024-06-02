33602bc407698d7a5d2bcbc65069861791f48000
PasukkaBig <o_kristina2000@mail.ru>
2024-06-02 17:08:42 +0700

    errors for no chat, chat del and db closing

--- database/errors.py
+++ database/errors.py
@@ -1,17 +1,38 @@
 class DatabaseException(Exception):
     pass
 
+
 class DBCreationError(DatabaseException):
     def __init__(self):
         self.message = f"Error occurred while creating database!"
         super().__init__(self.message)
+
+
+class DBClosingError(DatabaseException):
+    def __init__(self):
+        self.message = f"Error occurred while closing database!"
+        super().__init__(self.message)
+
 
 class ChatCreationError(DatabaseException):
     def __init__(self, id):
         self.message = f"Error occurred while creating chat {id}!"
         super().__init__(self.message)
 
+
+class ChatDeletionError(DatabaseException):
+    def __init__(self, id):
+        self.message = f"Error occurred while deleting chat {id}!"
+        super().__init__(self.message)
+
+
 class ChatPathError(DatabaseException):
     def __init__(self, id):
         self.message = f"Error occurred while selecting file path of chat {id}!"
+        super().__init__(self.message)
+
+
+class NoChatError(DatabaseException):
+    def __init__(self, id):
+        self.message = f"No chat {id} in database!"
         super().__init__(self.message)
