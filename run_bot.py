7cf9a41f2e05ca1f1817d02fd0f84c64ce212ed1
PasukkaBig <o_kristina2000@mail.ru>
2024-06-02 20:05:17 +0700

    creating admin and closing db

--- run_bot.py
+++ run_bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import hashlib
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram_dialog import Dialog
@@ -8,7 +9,7 @@ from aiogram_dialog import setup_dialogs
 from bot.bot import router, bot_logger
 from bot.commands import set_commands
 from bot.windows import file_list_window, remove_files_window
-from database.database import create_database
+from database.database import create_database, create_admin, close_db
 from database.errors import DBCreationError
 
 
@@ -18,12 +19,17 @@ async def start():
     info = await bot.get_me()
     try:
         await create_database()
+        psw = os.getenv('ADM_PSW')
+        await create_admin(hashlib.md5(psw.encode('utf-8')).hexdigest())
         bot_logger.logger.info(f"Created database.")
     except DBCreationError as e:
         bot_logger.logger.warning(f"Error occurred while creating database.")
         bot_logger.logger.exception(e)
+    except Exception as e:
+        await close_db()
+        bot_logger.logger.exception(e)
+        bot_logger.logger.critical(f"Closed db connection.")
 
-    bot_logger.logger.info(f"Started bot @{info.username} id={bot.id}.")
     dp = Dispatcher()
     dp.include_router(router)
     dialog = Dialog(file_list_window, remove_files_window)
@@ -31,10 +37,12 @@ async def start():
     setup_dialogs(dp)
     await set_commands(bot)
     try:
+        bot_logger.logger.info(f"Started bot @{info.username} id={bot.id}.")
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
         bot_logger.logger.critical(f"Closed bot @{info.username} id={bot.id}.")
+        close_db()
 
 if __name__ == "__main__":
     try:
