487bac7af2a5649da6b8a97800e4c81df266d4ab
PasukkaBig <o_kristina2000@mail.ru>
2024-06-03 20:12:08 +0700

    started bot tests

--- tests/bot/bot_test.py
+++ tests/bot/bot_test.py
@@ -1,3 +1,19 @@
+import pytest
+from unittest.mock import AsyncMock
+
+from bot.bot import handle_start, ds_controller
+from bot.keyboards import reply_keyboard
+
+
+@pytest.mark.asyncio
+async def test_start():
+    message = AsyncMock()
+    await handle_start(message)
+
+    message.answer.assert_called_with(ds_controller.metadata["info"]["start_info"].replace(
+        "_", "\\_"), reply_markup=reply_keyboard)
+
+
 '''
 TODO
 
