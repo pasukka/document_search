7a706d15210c8287e6ec771784dc4da564cc86b4
PasukkaBig <o_kristina2000@mail.ru>
2024-06-03 23:42:10 +0700

    started bot tests

--- tests/bot/bot_test.py
+++ tests/bot/bot_test.py
@@ -1,24 +1,32 @@
 import pytest
-from unittest.mock import AsyncMock
 
+from aiogram import types, Dispatcher
 from bot.bot import handle_start, ds_controller
-from bot.keyboards import reply_keyboard
+from database.database import create_database, close_db
+
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+def setup(dp: Dispatcher):
+    dp.register_message_handler(echo)
+
+
+class MockMessage:
+    def __init__(self, text, chat_id):
+        self.text = text
+        self.chat = types.Chat(id=chat_id, type='private')
+        self._answer = ""
+
+    async def answer(self, text: str, parse_mode, reply_markup):
+        self._answer = text
 
 
 @pytest.mark.asyncio
-async def test_start():
-    message = AsyncMock()
-    await handle_start(message)
+async def test_echo_handler():
+    text_mock = "/start"
+    await create_database()
+    message_mock = MockMessage(text=text_mock, chat_id=1223)
+    await handle_start(message=message_mock)
+    assert message_mock._answer == ds_controller.metadata["info"]["start_info"].replace("_", "\\_")
+    await close_db()
 
-    message.answer.assert_called_with(ds_controller.metadata["info"]["start_info"].replace(
-        "_", "\\_"), reply_markup=reply_keyboard)
-
-
-'''
-TODO
-
-https://www.youtube.com/watch?v=w0l4S838Idc
-
-https://dev.to/blueset/how-to-write-integration-tests-for-a-telegram-bot-4c0e
-
-'''
