3a8a8b5067bd244a1251fd62c7cc1f427f936e68
PasukkaBig <o_kristina2000@mail.ru>
2024-06-13 21:37:08 +0700

    changed funcs in logger

--- run_bot.py
+++ run_bot.py
@@ -25,25 +25,25 @@ async def start():
         await create_database()
         psw = os.getenv('ADM_PSW')
         await create_admin(hashlib.md5(psw.encode('utf-8')).hexdigest())
-        bot_logger.logger.info(f"Created database.")
+        bot_logger.info(f"Created database.")
     except DBCreationError as e:
-        bot_logger.logger.warning(f"Error occurred while creating database.")
-        bot_logger.logger.exception(e)
+        bot_logger.warning(f"Error occurred while creating database.")
+        bot_logger.exception(e)
     except Exception as e:
         await close_db()
-        bot_logger.logger.exception(e)
-        bot_logger.logger.critical(f"Closed db connection.")
+        bot_logger.exception(e)
+        bot_logger.critical(f"Closed db connection.")
 
     dialog = Dialog(file_list_window, remove_files_window)
     dp.include_router(dialog)
     setup_dialogs(dp)
     await set_commands(bot)
     try:
-        bot_logger.logger.info(f"Started bot @{info.username} id={bot.id}.")
+        bot_logger.info(f"Started bot @{info.username} id={bot.id}.")
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
-        bot_logger.logger.critical(f"Closed bot @{info.username} id={bot.id}.")
+        bot_logger.critical(f"Closed bot @{info.username} id={bot.id}.")
         await close_db()
 
 if __name__ == "__main__":
@@ -52,4 +52,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\nOkay, I'm stopping...")
     finally:
-        bot_logger.logger.critical("Bot stopped.")
+        bot_logger.critical("Bot stopped.")
